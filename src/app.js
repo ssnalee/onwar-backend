e59dad182ab19b4d481084b1077453f678b18e1c
ssnalee <97poolbbang@naver.com>
2025-06-10 16:37:19 +0900

    app 순서 변경 / 렌더 설정 중

--- src/app.js
+++ src/app.js
@@ -15,9 +15,9 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
+app.use(express.json());
 
 app.use((req, res, next) => {
     console.log(`[${new Date().toISOString()}] ${req.method} ${req.originalUrl}`);
